211c2e1c5f42fe2ea2930613edea72b23aea01ec
Tej1324 <tejakodiyala01@gmail.com>
2026-03-06 17:05:24 +0530

    3.O

---
+++ calculatesum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int calSum(int n){
+    int sum = 0;
+
+    for(int i=1; i<=n; i++){
+        sum += i;
+    }
+    return sum;
+
+}
+
+int main(){
+    cout << calSum(10) << endl;
+    cout << calSum(57648) << endl;
+    return 0;
+
+}
